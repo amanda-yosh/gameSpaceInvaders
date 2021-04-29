c1d39b6d67e2a1504d5d6094cf697eec75da9aec
Amanda Maria <amandamca.aa@gmail.com>
2021-04-29 11:35:49 -0300

    adding JLabel and background

--- src/br/display/Display.java
+++ src/br/display/Display.java
@@ -3,12 +3,15 @@ package br.display;
 import java.awt.Canvas;
 import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Display {
 
 	private JFrame jframe;
 	private Canvas canvas;
+	private JLabel background;
 	
 	public Display(String title, int width, int height) {
 		canvas = new Canvas();
@@ -16,8 +19,12 @@ public class Display {
 		canvas.setMaximumSize(new Dimension(width, height));
 		canvas.setMinimumSize(new Dimension(width, height));
 		
+		background = new JLabel(new ImageIcon("src\\fundo.png"), 0);
+		
 		jframe = new JFrame(title);
 		jframe.add(canvas);
+		jframe.add(background);
+		
 		jframe.pack();
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setResizable(false);
