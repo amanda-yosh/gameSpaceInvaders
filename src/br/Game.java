c38cafd6087041e541d32a0a3648c2764a7e87f0
Amanda Maria <amandamca.aa@gmail.com>
2021-04-29 12:54:59 -0300

    adding a Thread to separate the painting from the app

--- src/br/Game.java
+++ src/br/Game.java
@@ -1,12 +1,47 @@
 package br;
 
-import br.display.Display;
+import java.awt.Graphics;
 
-public class Game {
+import br.display.Display;
+import br.states.StateManager;
+
+public class Game implements Runnable{
 
 	private Display display;
+	private Thread thread;
+	private boolean running = false;
 	
 	public Game() {
 		display = new Display("Space Invaders", 1200, 1000);
+	}
+
+	@Override
+	public void run() {
+		//ELEMENTO QUE SERÁ RODADO NA THREAD SEPARADA
+		while(running) {
+			System.out.println("Esta rodando");
+		}
+	}
+	
+	/*MÉTODOS PARA PODERMOS LIGAR E DESLIGAR A THREAD
+	* void start();
+	* void stop();
+	* utilizando o modificador synchronized
+	*/
+	public synchronized void start() {
+		if (thread != null) return; //SE JÁ EXISTIR UM OBJETO, RETORNAMOS E NÃO INSTANCIAMOS UM NOVO OBJETO
+		thread = new Thread(this); //NAO EXISTINDO, INSTANCIAMOS UM E VAI QUERER SABER O TARGET, A CLASSE QUE TEM O MÉTODO RUN, ESTA.
+		thread.start();
+		running = true;
+	}
+	
+	public synchronized void stop() {
+		if (thread == null) return;
+		try {
+			thread.join(); //IRA JUNTAR COM A APLICACAO PRINCIPAL
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
